c8b44f5d3c66ec8233a3039c853b984138b16879
Saray Obama <plankjim3@gmail.com>
2023-03-30 18:14:45 +0300

    complete

---
+++ src/API.js
@@ -0,0 +1,4 @@
+export const API = {
+	openWeather: '7efa332cf48aeb9d2d391a51027f1a71',
+	mapboxAPIKey: "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q",
+}
